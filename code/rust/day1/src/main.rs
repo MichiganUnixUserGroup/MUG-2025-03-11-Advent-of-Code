36c7f2a21f9238a7950698ab5cc5fffc534169b3
Wolf <Wolf@zv.cx>
2025-01-09 16:38:42 -0500

    Rust: more flexible types and _lots_ of comments

--- code/rust/day1/src/main.rs
+++ code/rust/day1/src/main.rs
@@ -2,27 +2,45 @@ use counter::Counter;
 use regex::Regex;
 use std::io::{self, BufRead};
 
-// TODO: add documentation strings
-// TODO: talk about why unwrap, or maybe expect, is the right thing to do in this program
-
 fn read_the_two_lists() -> (Vec<i32>, Vec<i32>) {
+    // No parameters.  We get all our data from stdin.
+    let stdin = io::stdin();
+
+    // We're returning two lists. They need to be mutable so we can push in all the values.
+    let mut left: Vec<i32> = vec![];
+    let mut right: Vec<i32> = vec![];
+
+    // Define a regular expression that captures two number strings separated by whitespace.
     let re = Regex::new(r"^(\d+)\s+(\d+)$").unwrap();
 
-    let stdin = io::stdin();
+    // Step 1: read the input
+
+    // Read all of stdin into a vector of strings, one string per line of input.
     let lines: Vec<String> = stdin
         .lock() // Locks stdin, providing a `BufRead`-capable type
         .lines() // Uses the `BufRead` trait
         .map(|line| line.expect("Failed to read line"))
         .collect();
 
-    let mut left: Vec<i32> = vec![];
-    let mut right: Vec<i32> = vec![];
+    // Step 2: step through the lines, grabbing two numbers from each; pushing them into the result
+    // lists.
 
     for line in lines {
+        // This whole loop is an example of me _not_ handling errors.  Just one `unwrap` after
+        // another.  This simple program calculates a simple result.  I probably don't need fancy
+        // error handling, so `unwrap` and friends is appropriate.
+
+        // Apply the regular expression to the current line.  `captures` lets you get to the captured
+        // strings.
         let captures = re.captures(&line).unwrap();
-        // I hate that this takes two statements. I should write a helper that returns a tuple
+
+        // I hate that this takes two statements. I should write a helper that returns a tuple.
+        // Pull the two strings out of the match.  `l` and `r` are string slices, each one a
+        // number.
         let l = captures.get(1).unwrap().as_str();
         let r = captures.get(2).unwrap().as_str();
+
+        // Parse the two strings into actual numbers. Add one to each list.
         left.push(l.parse().unwrap());
         right.push(r.parse().unwrap());
     }
@@ -30,23 +48,46 @@ fn read_the_two_lists() -> (Vec<i32>, Vec<i32>) {
     (left, right)
 }
 
-fn distance_between_the_two_lists(lhs: &Vec<i32>, rhs: &Vec<i32>) -> i32 {
-    // must clone because we will sort the lists
-    let mut left: Vec<i32> = lhs.clone();
-    let mut right: Vec<i32> = rhs.clone();
-    // so annoying that sorting doesn't return a value ... it can't be used in a chain above
+fn distance_between_the_two_lists(lhs: &[i32], rhs: &[i32]) -> i32 {
+    // The problem statement says the lists are sorted.  Sorting modifies in place, so we must 
+    // make something mutable.
+    let mut left: Vec<i32> = lhs.to_vec();
+    let mut right: Vec<i32> = rhs.to_vec();
+
+    // Step 1: sort the input lists
+
+    // So annoying that sorting doesn't return a value ... it can't be used in a chain above.
     left.sort_unstable();
     right.sort_unstable();
 
+    // Step 2: add up the distance at each step
+
+    // We're stepping through both lists at the same time.  So each list needs `.iter()`.  `.zip()`
+    // works on two lists.  For each step, `.zip()` returns a tuple of two values, one from each
+    // list.  This function is about the difference between the two sides, so `.map()` that
+    // subtracts to find that difference, but we need it to be positive, so `.abs()`.  Finally,
+    // `.sum()` adds them all up.  That sum is the final answer.
     left.iter().zip(right.iter()).map(|(l, r)| (l - r).abs()).sum()
 }
 
-fn similarity_score(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
+fn similarity_score(left: &[i32], right: &[i32]) -> i32 {
+    // Step 1: count up the values on the right side.  That's one of the two multiplicands in the
+    // similarity score
+
+    // A Counter takes any iterable and builds a dictionary where the keys are the things you
+    // stepped through in the iterator, and the values are how many times you saw that thing.
     let right_side_counts = right.iter().collect::<Counter<_>>();
+
+    // Step 2: for each value in the left list, multiply by the number of times that value appears
+    // in the right list.  Don't "uniquify" the left list.  If we hit the same number from the left
+    // list multiple times, it counts every time.
 
     let mut similarity = 0;
     for l in left {
+        // Mostly I'm omitting error handling in this implementation.  Maybe I shouldn't.  I could
+        // just `.unwrap()` the result of `.get`.  I'm trying it the other way here.
         if let Some(&count) = right_side_counts.get(&l) {
+            // `count` is a u32.  To multiply, `l` and `count` must be the same type.
             similarity += l * count as i32;
         }
     }
@@ -55,12 +96,15 @@ fn similarity_score(left: &Vec<i32>, right: &Vec<i32>) -> i32 {
 }
 
 fn main() {
-    // cargo run < your_data_file    to execute the code normally (it reads from stdin)
-    let (left, right) = read_the_two_lists();
+    // Vec<i32> to own the values; but everybody else uses slices.
+    let (left_vec, right_vec) = read_the_two_lists();
+    let (left, right) = (left_vec.as_slice(), right_vec.as_slice());
+
     println!(
         "Day 1, part 1: the distance between the two lists is {}.",
         distance_between_the_two_lists(&left, &right)
     );
+
     println!(
         "Day 1, part 2: the similarity score for the two lists is {}.",
         similarity_score(&left, &right)
@@ -69,24 +113,22 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    // cargo test     to run the tests
     use super::*;
+
+    // These are the lists from the problem description.  I'll compare against the answers given
+    // in the same place.  Don't need Vec<i32> here.  I can define the inputs as slices directly.
+    const LEFT: [i32; 6] = [3, 4, 2, 1, 3, 3];
+    const RIGHT: [i32; 6] = [4, 3, 5, 3, 9, 3];
 
     #[test]
     fn test_day1_part1() {
-        let left = vec![3, 4, 2, 1, 3, 3];
-        let right = vec![4, 3, 5, 3, 9, 3];
-
-        let distance = distance_between_the_two_lists(&left, &right);
+        let distance = distance_between_the_two_lists(&LEFT, &RIGHT);
         assert_eq!(distance, 11);
     }
 
     #[test]
     fn test_day1_part2() {
-        let left = vec![3, 4, 2, 1, 3, 3];
-        let right = vec![4, 3, 5, 3, 9, 3];
-
-        let similarity = similarity_score(&left, &right);
+        let similarity = similarity_score(&LEFT, &RIGHT);
         assert_eq!(similarity, 31);
     }
 }

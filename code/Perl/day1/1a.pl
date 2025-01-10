95eb02d29d52a26341044fa92690206871dc82f0
Jim McQuillan <jam@Avairis.com>
2025-01-10 16:43:42 -0500

    Perl: cleaned up the code and added comments

--- code/Perl/day1/1a.pl
+++ code/Perl/day1/1a.pl
@@ -1,34 +1,59 @@
 #!/usr/bin/perl -w
 
+#
+# This Perl script expects the data to be in a file passed on the command line
+#
+# Easiest way to run this is by doing:
+#
+#    ./1a.pl  data_sample.txt
+#
+# Once you run it with the sample data then try it
+# with the real data file.
+#
+# Created by: James McQuillan <jam@McQuil.com>
+#
+
 use strict;
 
+my $filename = $ARGV[0] or die("Must specify filename as the only argument!\n");
+
+#
+# @col1 is an array to hold all of the values in column 1 of the input data
+#
 my @col1;
+
+#
+# @col2 is an array to hold all of the values in column 2 of the input data
+#
 my @col2;
 
-open( my $FH, '<', 'data.txt' ) or die( "Failed to open file: $!\n" );
+open( my $FH, '<', $filename ) or die( "Failed to open $filename: $!\n" );
 
-#while( my $line = <$FH> ){
-#  my ( $c1, $c2 ) = split( /\s+/, $line );
-#  push @col1, $c1;
-#  push @col2, $c2;
-#}
-
+#
+# Loop through the lines of the data file and build the Arrays
+#
 while( my $line = <$FH> ){
-  if( $line =~ m/(\d+)\s+(\d+)$/ ){
-    push @col1, $1;
-    push @col2, $2;
-  }
+  my ( $left, $right ) = split( /\s+/, $line );
+
+  push @col1, $left;
+  push @col2, $right;
 }
 
 close( $FH );
 
+#
+# Sort both arrays
+#
 @col1 = sort( @col1 );
 @col2 = sort( @col2 );
 
 my $sum = 0;
 
+#
+# Loop through the arrays and add up the differences
+#
 for( my $i = 0; $i < @col1; $i++ ){
   $sum += abs( $col1[$i] - $col2[$i] );
 }
 
-print("sum: $sum\n");
+print( "\nTotal distance between lists: $sum\n\n" );
--- code/Perl/day1/1b.pl
+++ code/Perl/day1/1b.pl
@@ -1,21 +1,58 @@
 #!/usr/bin/perl -w
 
+#
+# This Perl script expects the data to be in a file passed on the command line
+#
+# Easiest way to run this is by doing:
+#
+#    ./1b.pl  data_sample.txt
+#
+# Once you run it with the sample data then try it
+# with the real data file.
+#
+# Created by: James McQuillan <jam@McQuil.com>
+#
+
 use strict;
 
+#
+# @col1 is an array to hold all of the values in column 1 of the input data
+#
 my @col1;
+
+#
+# %col2 is a hash where we keep track of the frequency of each value.
+# The key is the value from column 2 of the input data and the value
+# is the number of times that key appears.
+#
 my %col2;
 
-open( my $FH, '<', 'data.txt' ) or die("Failed to open file: $!\n");
+my $filename = $ARGV[0] or die("Must specify filename as the only argument!\n");
 
+open( my $FH, '<', $filename ) or die( "Failed to open $filename: $!\n" );
+
+#
+# Loop through the lines of the data file and build the Array and Hash
+#
 while( my $line = <$FH> ){
-  if( $line =~ m/(\d+)\s+(\d+)$/ ){
-    push @col1, $1;
-    if( exists( $col2{$2} ) ){
-      $col2{$2} += 1;
-    }
-    else{
-      $col2{$2} = 1;
-    }
+  my ( $left, $right ) = split( /\s+/, $line );
+
+  push @col1, $left;
+
+  #
+  # See if the key exists in the hash
+  #
+  if( exists( $col2{$right} ) ){
+    #
+    # If it already exists then increment the counter
+    #
+    $col2{$right} += 1;
+  }
+  else{
+    #
+    # Otherwise, create the hash entry and initialize it to 1
+    #
+    $col2{$right} = 1;
   }
 }
 
@@ -23,10 +60,17 @@ close( $FH );
 
 my $sum = 0;
 
+#
+# Loop through the @col1 array and for each number that appears
+# in the hash, do the multipication and add it to the sum
+#
 foreach my $entry ( @col1 ){
   if( exists( $col2{$entry} ) ){
+    #
+    # Multiply the value by the frequency and add it to the sum
+    #
     $sum += $entry * $col2{$entry};
   }
 }
 
-print("sum: $sum\n");
+print( "\nTotal simularity score: $sum\n\n" );

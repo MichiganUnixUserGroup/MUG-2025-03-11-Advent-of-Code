5bd7e6deb35b298a679a2c4e4318bc10eabb70e6
Jim McQuillan <jam@Avairis.com>
2025-01-11 16:46:02 -0500

    SQL: cleaned up the code a bit, using a single UNNEST() instead of multiple

--- code/SQL/jammcq/day1/1a.sql
+++ code/SQL/jammcq/day1/1a.sql
@@ -1,27 +1,37 @@
-\set QUIET ON
-
+--
+-- SQL implementation of day 1 part Aa
+--
+-- This *probably* only works in Postgresql, as other databases don't have the *UNNEST* function.
+--
+-- Run this by using the psql command and redirecting the data file as stdin
+--
+-- Example:
+--
+--   psql -f 1a.sql <data_sample.txt
+--
+--
+-- Created by: James McQuillan <jam@McQuil.com>
+--
+-- -----------------------------------------------------------------------------
+--
+-- Create a table to hold the data so we can query it
+--
 CREATE TEMP TABLE data ( line TEXT );
 
 --
--- Pick which input file you want to use
+-- Get the data into the database by getting it from STDIN of the psql command
 --
-
-\copy data FROM 'data_sample.txt'
-
---\copy data FROM 'data.txt'
-
+\copy data FROM PSTDIN
 
 SELECT SUM( bar.distance ) AS "Total distance between lists"
   FROM ( SELECT abs( foo.col1 - foo.col2 ) AS distance
-          FROM ( SELECT UNNEST( ARRAY( ( SELECT regexp_replace( data.line, '(\d+)\s+(\d+)', '\1' )::INT
-                                           FROM data
-                                          ORDER BY 1
-                                       ) )
-                              ) AS col1,
-                        UNNEST( ARRAY( ( SELECT regexp_replace( data.line, '(\d+)\s+(\d+)', '\2' )::INT
-                                           FROM data
-                                          ORDER BY 1
-                                       ) )
-                              ) AS col2
-               ) AS foo
-       ) AS bar;
+           FROM UNNEST( ARRAY( ( SELECT regexp_replace( data.line, '(\d+)\s+(\d+)', '\1' )::INT
+                                   FROM data
+                                  ORDER BY 1
+                             ) ),
+                        ARRAY( ( SELECT regexp_replace( data.line, '(\d+)\s+(\d+)', '\2' )::INT
+                                   FROM data
+                                  ORDER BY 1
+                             ) )
+                      ) AS foo( col1, col2 )
+        ) AS bar;
--- code/SQL/jammcq/day1/1b.sql
+++ code/SQL/jammcq/day1/1b.sql
@@ -1,32 +1,43 @@
-\set QUIET ON
-
+--
+-- SQL implementation of day 1 part B
+--
+-- This *probably* only works in Postgresql, as other databases don't have the *UNNEST* function.
+--
+-- Run this by using the psql command and redirecting the data file as stdin
+--
+-- Example:
+--
+--   psql -f 1b.sql <data_sample.txt
+--
+--
+-- Created by: James McQuillan <jam@McQuil.com>
+--
+-- -----------------------------------------------------------------------------
+--
+-- Create a table to hold the data so we can query it
+--
 CREATE TEMP TABLE data ( line TEXT );
 
 --
--- Pick which input file you want to use
+-- Get the data into the database by getting it from STDIN of the psql command
 --
-
-\copy data FROM 'data_sample.txt'
-
---\copy data FROM 'data.txt'
+\copy data FROM PSTDIN
 
 CREATE TEMP TABLE columns (
   col1 INT,
   col2 INT
 );
 
-
 INSERT INTO columns (
   col1,
   col2
-) SELECT UNNEST( ARRAY( ( SELECT regexp_replace( data.line, '(\d+)\s+(\d+)', '\1' )::INT
-                            FROM data
-                      ) )
-               ) AS col1,
-         UNNEST( ARRAY( ( SELECT regexp_replace( data.line, '(\d+)\s+(\d+)', '\2' )::INT
-                            FROM data
-                        ) )
-               ) AS col2;
+) SELECT * FROM UNNEST( ARRAY( ( SELECT regexp_replace( data.line, '(\d+)\s+(\d+)', '\1' )::INT
+                                   FROM data
+                               ) ),
+                        ARRAY( ( SELECT regexp_replace( data.line, '(\d+)\s+(\d+)', '\2' )::INT
+                                   FROM data
+                               ) )
+                      ) AS foo;
 
 SELECT SUM(bar.similarity_score) AS "Total simularity score"
   FROM (

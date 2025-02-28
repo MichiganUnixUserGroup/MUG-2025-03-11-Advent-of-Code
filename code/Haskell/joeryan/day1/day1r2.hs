701513efc1592e9b3ca6c2feea4ea80d9596dc5c
Joe Ryan <joe@clanryan.us>
2025-02-27 20:49:54 -0500

    moved commented files to day directory

--- code/Haskell/joeryan/day1/day1r2.hs
+++ code/Haskell/joeryan/day1/day1r2.hs
@@ -4,19 +4,24 @@ import System.Environment (getArgs)
 import Data.List (sort)
 
 main = do
+  -- get file name and read in contents
   args <- getArgs
-
   let inFile = head args
   file1contents <- readFile inFile
+  
+  -- create a sorted list of tuples from the file contents
   let colLines = convertLines (lines file1contents)
 
+  -- sum the absolute values of each tuple using list comprehension
   let result = sum [ abs (x - y)| (x,y) <- colLines]
-
   print result
 
+
+-- function to take the lines from the file and return an list of sorted tuples
 convertLines :: [String] -> [(Integer, Integer)]
 convertLines inputList = do
   let firstList = sort [ read x | x:xs <- map words inputList]
-  let secondList = sort [ read (unwords xs) | x:xs <- map words inputList]
+  let secondList = sort [ read (head xs) | x:xs <- map words inputList]
+  -- return a list of tuples created with an element from each sorted list
   zip firstList secondList
 

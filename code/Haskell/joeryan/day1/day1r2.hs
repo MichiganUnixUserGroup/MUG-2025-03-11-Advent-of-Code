da1e2353f1db607f6e89439414db23a009a4745a
Joe Ryan <joe@clanryan.us>
2025-03-10 12:28:13 -0400

    completed day 1 section 2

--- code/Haskell/joeryan/day1/day1r2.hs
+++ code/Haskell/joeryan/day1/day1r2.hs
@@ -2,6 +2,7 @@
 import System.IO
 import System.Environment (getArgs)
 import Data.List (nub, sort)
+import Data.Map as Map
 
 main = do
   -- get file name and read in contents
@@ -13,32 +14,32 @@ main = do
   let colLines = convertLines (lines file1contents)
   -- sum the absolute values of each tuple using list comprehension
   let result = sum [ abs (x - y)| (x,y) <- colLines]
-  print "sum for part 1"
-  print result
+  print ("Sum for part 1: " ++ show result)
+  
 
   -- create two lists of integers from the file contents
   let unzipList = unzip colLines
   -- create zero value map from firstList
-  let firstList = getFirst unzipList
-  -- print ("map: " ++ show mapList)
-  
-  -- for each entry in secondList, if in first list then add to result
-  let resultList = [x | x <- getSecond unzipList, x `elem` firstList  ]
-  print resultList
-  -- take multiply each k,v and add to sum 
-  
-  -- print similarity
+  let firstList = getFirstList (unzip colLines)
+  let secondList = getSecondList (unzip colLines)
+  -- print ("List: " ++ show firstList ++ show secondList)
+    
+  -- multiply each element by the count of occurrances in secondList add to sum 
+  let similarity = sum [ x * (length (Prelude.filter (\target -> target == x) secondList )) | x <- firstList]
+  print ("Similarity: " ++ show similarity)
+
 
 -- function to take the lines from the file and return an list of sorted tuples
-convertLines :: [String] -> [(Integer, Integer)]
+convertLines :: [String] -> [(Int, Int)]
 convertLines inputList = do
-  let firstList = Data.List.sort [ read x | x:xs <- map words inputList]
-  let secondList = Data.List.sort [ read (head xs) | x:xs <- map words inputList]
+  let firstList = Data.List.sort [ read x | x:xs <- Prelude.map words inputList]
+  let secondList = Data.List.sort [ read (head xs) | x:xs <- Prelude.map words inputList]
   -- return a list of tuples created with an element from each sorted list
   zip firstList secondList
 
-getFirst :: ([Integer], [Integer]) -> [Integer]
-getFirst (x, _) = x
+getFirstList :: ([Int], [Int]) -> [Int]
+getFirstList (x, _) = x
 
-getSecond :: ([Integer], [Integer]) -> [Integer]
-getSecond (x, _) = x
+getSecondList :: ([Int], [Int]) -> [Int]
+getSecondList (_, x) = x
+
